fd88255b08589087bf7536a4f03241dff6b50fdd
Ludwig Rahlff <ludwig.rahlff@student.uni-luebeck.de>
2023-11-22 10:34:38 +0100

    init commit with annikas version

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+setup(name='matrixmodule',
+      version='0.1',
+      description='A plugin to read and write matrix',
+      author='Annika Heidmann',
+      packages=['matrixmodule'],
+      zip_safe=False)
